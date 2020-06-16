73cc213943111f1d306850e3499bd82aeaf2e6db
Luis Diego Hernández <lhernand5@publicisgroupe.net>
2020-06-15 18:38:12 -0600

    New Site Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-	title: 'Full-Stack Bootcamp',
+	title: 'Gatsby App',
 	author: 'Luis Diego Hernández Araya'
   },
   plugins: [
